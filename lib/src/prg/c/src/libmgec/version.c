85e33f29b40a8250114e0d622a126ebca5c6a5a8
Mark Grant <m.grant.prg@gmail.com>
2018-06-04 14:26:27 +0100

    lib: Make ChangeLog in version.c for that file only.
    Signed-off-by: Mark Grant <m.grant.prg@gmail.com>

--- lib/src/prg/c/src/libmgec/version.c
+++ lib/src/prg/c/src/libmgec/version.c
@@ -10,7 +10,7 @@
  * Released under the GPLv3 only.\n
  * SPDX-License-Identifier: GPL-3.0
  *
- * @version _v1.1.3 ==== 02/01/2018_
+ * @version _v1.0.4 ==== 04/06/2018_
  */
 
 /* **********************************************************************
@@ -20,54 +20,9 @@
  * Date		Author	Version	Description				*
  *									*
  * 07/08/2015	MG	1.0.1	First release.				*
- * 11/08/2015	MG	1.0.2	Add functions delnode() & freenode().	*
- * 18/09/2015	MG	1.0.3	Change function names to XXX_bt_node().	*
- *				Change find_bt_node() to		*
- *				get_counter_bt_node() as it returns the	*
- *				count.					*
- *				Add find_bt_node(), upd_bt_node().	*
- *				Change free_btree() to return NULL.	*
- *				Add unique flag to add_bt_node().	*
- *				Improve overall use of btree_errno.	*
- *				Modify del_bt_node() to decrement the	*
- *				node counter instead of deleting the	*
- *				node if the counter is > 1, (i.e.	*
- *				duplicates are allowed).		*
- *				Add function parameter checking.	*
- *				Change defined flags and errors to be	*
- *				easily identifiable as btree elements.	*
- * 03/11/2015	MG	1.0.4	Change from bt and btree names to bst	*
- *				and bstree as this is a binary search	*
- *				tree, not a b-tree.			*
- * 11/11/2015	MG	1.0.5	Change for extraction of bstree.h from	*
- *				now umbrella listsandsorts.h.		*
- *				Add support for singly linked lists.	*
- *				Elevate errno defs to listsandsorts.h.	*
- *				Add base.c for common functions and	*
- *				definitions.				*
- * 02/05/2016	MG	1.0.6	Add singly linked list functionality.	*
- * 06/05/2016	MG	1.0.7	Add doubly linked list functionality.	*
- * 16/07/2016	MG	1.0.8	Move towards kernel coding style.	*
- * 17/07/2016	MG	1.0.9	Further coding style change.		*
- * 12/03/2017	MG	1.0.10	get_counter_bst_node() now returns -1	*
- *				on error.				*
- * 03/05/2017	MG	1.1.0	Return NULL pointer, -1 or error number	*
- *				when possible on error.			*
- *				Apply coding standards from template	*
- *				atclstv v1.0.0				*
- *				Add buffer, message and memory		*
- *				functions.				*
- *				Introduce library-wide mge_errno	*
- *				variable and error handling.		*
- *				Add bst trace function to provide node	*
- *				coordinates.				*
- *				Add exhaustive bstree tests.		*
- *				Introduce concept and support for a	*
- *				discreet bst object.			*
- * 05/11/2017	MG	1.1.1	Add SSH error message.			*
- * 				Add Doxygen comments.			*
- * 09/11/2017	MG	1.1.2	Add SPDX license tags.			*
- * 02/01/2018	MG	1.1.3	Move to new source directory structure.	*
+ * 09/11/2017	MG	1.0.2	Add SPDX license tags.			*
+ * 02/01/2018	MG	1.0.3	Move to new source directory structure.	*
+ * 04/06/2018	MG	1.0.4	Make this ChangeLog for this file only.	*
  *									*
  ************************************************************************
  */
